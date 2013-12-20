5277b7a5a52c334acb7193f30138f2daa4ab3377
hhquangvu <admin@Macintosh.local>
2013-12-20 20:05:54 +0700

    update

--- SeaStoreApp/public/js/index.js
+++ SeaStoreApp/public/js/index.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 	});
 
 	$('.vh-image-item-bg').bind('mouseover', function() {
-		$(this).next('.vh-image-item-info').show();
+		$(this).next('.vh-image-item-info').fadeIn();
 	});
 
 	$('.vh-image-item-info').bind('mouseleave', function() {
-		$('.vh-image-item-info').hide();
+		$('.vh-image-item-info').fadeOut();
 	});
 
 });
