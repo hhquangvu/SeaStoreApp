787f86962d41004d0e1a216a06042af637ac2454
hhquangvu <admin@Macintosh.local>
2013-12-22 10:57:56 +0700

    Upadate format price

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,25 @@
-$(document).ready(function() {
+$(document).ready(
+		function() {
 
-	$(window).bind('load', function() {
-	});
+			$(window).bind('load', function() {
+				$('.vh-image-item-price').each(function(i) {
+					$(this).text(formatPrice($(this).text()) + ' đ');
+				});
 
-	$('.vh-image-item-bg').bind('mouseover', function() {
-		$(this).next('.vh-image-item-info').fadeIn();
-	});
+			});
 
-	$('.vh-image-item-info').bind('mouseleave', function() {
-		$('.vh-image-item-info').fadeOut();
-	});
+			$('.vh-image-item-bg').bind('mouseover', function() {
+				$(this).next('.vh-image-item-info').fadeIn();
+			});
 
-});
+			$('.vh-image-item-info').bind('mouseleave', function() {
+				$('.vh-image-item-info').fadeOut();
+			});
+
+			function formatPrice(input) {
+				var output = input.substr(0, input.length - 3) + ","
+						+ input.substr(input.length - 3);
+				return output;
+			}
+
+		});
